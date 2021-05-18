d3b59cd99356af5d768dd15da6b1b301b5d5d87e
Kath <kathlene.tajonera@gmail.com>
2021-05-18 20:54:33 +0800

    desktop ui done

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ const navBar = document.querySelector(".nav");
 const navToggle = document.querySelector(".nav__toggle");
 const navMenu = document.querySelector(".nav__menu");
 const navItems = document.querySelectorAll(".nav__item");
+const editorIllustrationImage = document.querySelector("#editor");
+const laptopIllustrationImage = document.querySelector("#laptop");
+let windowWidth = window.innerWidth;
+
+updateImageSource(windowWidth);
 
 navToggle.addEventListener("click", () => {
     const isOpen = navBar.classList.contains("nav--active");
@@ -26,7 +31,41 @@ navItems.forEach(item => {
             updateClass("add", "active", item);
         }
     })
+});
+
+window.addEventListener("resize", () => {
+    if (windowWidth !== window.innerWidth) {
+        windowWidth = window.innerWidth;
+    }
+
+    if (windowWidth > 768) {
+        updateClass("remove", "active", navBar);
+        updateImageSource(windowWidth)
+    } else {
+        updateImageSource(windowWidth)
+    }
 })
+
+function updateImageSource(width) {
+    const imageSources = {
+        editor: {
+            desktop: "images/illustration-editor-desktop.svg",
+            mobile: "images/illustration-editor-mobile.svg"
+        },
+        laptop: {
+            desktop: "images/illustration-laptop-desktop.svg",
+            mobile: "images/illustration-laptop-mobile2.svg"
+        }
+    }
+
+    if (width >= 768) {
+        editorIllustrationImage.src = imageSources.editor.desktop;
+        laptopIllustrationImage.src = imageSources.laptop.desktop;
+    } else {
+        editorIllustrationImage.src = imageSources.editor.mobile;
+        laptopIllustrationImage.src = imageSources.laptop.mobile;
+    }
+}
 
 function updateClass(method, className, element) {
     const blockName = element.classList[0];
